41d2a2f9a00f34069dacb838ee6fcb427939dce3
Andrew Kane <andrew@chartkick.com>
2019-11-24 16:38:03 -0800

    Use FFI to debug

--- lib/thundersvm.rb
+++ lib/thundersvm.rb
@@ -1,4 +1,5 @@
 # stdlib
+require "ffi"
 require "fiddle/import"
 require "fileutils"
 require "tempfile"
--- lib/thundersvm/ffi.rb
+++ lib/thundersvm/ffi.rb
@@ -1,19 +1,7 @@
 module ThunderSVM
   module FFI
-    extend Fiddle::Importer
+    extend ::FFI::Library
 
-    libs = ThunderSVM.ffi_lib.dup
-    begin
-      dlload libs.shift
-    rescue Fiddle::DLError => e
-      retry if libs.any?
-      raise e if ENV["THUNDERSVM_DEBUG"]
-      raise LoadError, "Could not find ThunderSVM"
-    end
-
-    extern "void thundersvm_train(int argc, char **argv)"
-    extern "void thundersvm_train_after_parse(char **option, int len, char *file_name)"
-    extern "void thundersvm_predict(int argc, char **argv)"
-    extern "void thundersvm_predict_after_parse(char *model_file_name, char *output_file_name, char **option, int len)"
+    ffi_lib ThunderSVM.ffi_lib
   end
 end
