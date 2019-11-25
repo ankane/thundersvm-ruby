4952a641c25c64095b073009aecbcf470e9397ae
Andrew Kane <andrew@chartkick.com>
2019-11-24 16:35:29 -0800

    Better ffi_lib logic

--- lib/thundersvm.rb
+++ lib/thundersvm.rb
@@ -15,7 +15,14 @@ module ThunderSVM
   class << self
     attr_accessor :ffi_lib
   end
-  self.ffi_lib = ["libthundersvm.so", "libthundersvm.dylib", "thundersvm.dll"]
+  self.ffi_lib =
+    if Gem.win_platform?
+      ["thundersvm.dll"]
+    elsif RbConfig::CONFIG["host_os"] =~ /darwin/i
+      ["libthundersvm.dylib"]
+    else
+      ["libthundersvm.so"]
+    end
 
   # friendlier error message
   autoload :FFI, "thundersvm/ffi"
