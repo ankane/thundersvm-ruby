6a5c6ab888d3b14e199488f133c16ec439c86544
Andrew Kane <andrew@ankane.org>
2025-04-19 02:47:56 -0700

    Removed unneeded code

--- test/thundersvm_test.rb
+++ test/thundersvm_test.rb
@@ -72,8 +72,6 @@ class ThunderSVMTest < Minitest::Test
   def test_numo
     skip if RUBY_PLATFORM == "java"
 
-    require "numo/narray"
-
     x = Numo::DFloat.cast([[1, 2], [3, 4], [5, 6], [7, 8]])
     y = Numo::Int32.cast([1, 1, 2, 2])
 
