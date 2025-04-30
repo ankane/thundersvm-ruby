7e4c8fd7d1ca79767cf2f0bfd3784669677752d8
Andrew Kane <andrew@ankane.org>
2025-04-30 14:54:44 -0700

    Added GC stress testing [skip ci]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,21 @@ require "minitest/autorun"
 require "minitest/pride"
 
 class Minitest::Test
+  def setup
+    # autoload before GC.stress
+    ThunderSVM::FFI if stress?
+
+    GC.stress = true if stress?
+  end
+
+  def teardown
+    GC.stress = false if stress?
+  end
+
+  def stress?
+    ENV["STRESS"]
+  end
+
   def assert_elements_in_delta(expected, actual, delta = 0.001)
     assert_equal expected.size, actual.size
     expected.to_a.zip(actual.to_a) do |exp, act|
